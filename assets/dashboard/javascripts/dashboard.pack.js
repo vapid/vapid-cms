1ec6c5307ed8340298110fa4828ad0f31ad2da2a
Adam Miller <adam@pages.me>
2019-11-12 21:30:26 -0800

    feat: Page Templates.
     - New page template construct.
     - Sections => Collections.
     - Updated runtime to use Handlebars AST.
     - Updated analyzer to user Handlebars AST.
     - Section => Setting.
     - Navigation data property.
     - Use native @ data props for private values.
     - Full partials support.
     - Custom page slug support.
     - Site nav UI in dashboard.

--- assets/dashboard/javascripts/dashboard.pack.js
+++ assets/dashboard/javascripts/dashboard.pack.js
@@ -1,3 +1,4 @@
+/* global document, window */
 const jQuery = require('jquery');
 window.$ = window.jQuery = jQuery;
 
@@ -23,3 +24,20 @@ require('./dashboard/wysiwyg');
 $.ajaxSetup({
   headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
 });
+
+/* eslint-disable no-param-reassign */
+const autoExpand = (field) => {
+  field.style.resize = 'none';
+  field.style.height = 'inherit';
+  const computed = window.getComputedStyle(field);
+  const height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+    + parseInt(computed.getPropertyValue('padding-top'), 10)
+    + field.scrollHeight
+    + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+    + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+  field.style.height = `${height}px`;
+};
+const init = () => [...document.querySelectorAll('textarea')].map(autoExpand);
+document.addEventListener('input', (event) => event.target.tagName.toLowerCase() === 'textarea' && autoExpand(event.target), false);
+document.addEventListener('turbolinks:load', init);
+window.addEventListener('DOMContentLoaded', init);
--- assets/dashboard/javascripts/dashboard/sortable.js
+++ assets/dashboard/javascripts/dashboard/sortable.js
@@ -14,7 +14,40 @@ document.addEventListener("turbolinks:load", () => {
       const { index: to } = e.detail.destination;
       const id = item.getAttribute('data-id');
 
-      $.post('./records/reorder', { id, from, to }).fail((err) => {
+      $.post('/dashboard/records/reorder', { id, from, to }).fail((err) => {
+        // TODO: probably need some better client-side error handling here
+        alert('Error: could not reorder records');
+      });
+    });
+  }
+});
+
+document.addEventListener("turbolinks:load", () => {
+  const $el = $('.menu.sortable');
+
+  if ($el.length) {
+    sortable($el, {
+      forcePlaceholderSize: false,
+      items: 'a',
+      placeholder: '<a class="item" style="height: 37px"></a>',
+    });
+
+    $el[0].addEventListener('sortupdate', (e) => {
+      const { item } = e.detail;
+      const id = item.getAttribute('data-id');
+      const { index: from } = e.detail.origin;
+      const { index: to } = e.detail.destination;
+
+      // Check if this element is a nav item.
+      let nav = false;
+      for (const el of item.parentElement.children) {
+        if (el.tagName.toLowerCase() === 'hr') { break; }
+        if (el === item) { nav = true; }
+      }
+      // If this is not a nav item, correct for the extra element in the sorting container.
+      // if (!nav) { to -= 1; }
+
+      $.post('/dashboard/records/reorder', { id, from, to, nav }).fail((err) => {
         // TODO: probably need some better client-side error handling here
         alert('Error: could not reorder records');
       });
