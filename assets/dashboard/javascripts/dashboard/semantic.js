c868fb20c4379ad415a6ec63bd14d0952b0bb62e
Adam Miller <adam@pages.me>
2019-12-16 11:09:53 -0800

    feat: Refactor TemplateCompiler.

--- assets/dashboard/javascripts/dashboard/semantic.js
+++ assets/dashboard/javascripts/dashboard/semantic.js
@@ -1,6 +1,24 @@
+/* globals document, $ */
 document.addEventListener("turbolinks:load", () => {
   // Semantic UI
   $('.ui.checkbox').checkbox();
   $('.ui.dropdown').dropdown();
   $('.ui.sortable.table').tablesort();
+});
+
+document.addEventListener("turbolinks:load", () => {
+  $(document.body).on('keyup', '.ui.dropdown.custom', (evt) => {
+    if (evt.keyCode !== 13) { return; }
+    const { target } = evt;
+    const { value } = target;
+    const select = evt.target.parentElement.querySelector('select');
+    const op = document.createElement('option');
+    op.innerText = value;
+    op.setAttribute('value', value);
+    op.setAttribute('selected', true);
+    select.insertBefore(op, select.firstChild);
+    target.value = '';
+    select.value = value;
+    $(select).dropdown();
+  });
 });
